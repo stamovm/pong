049454b1efcd44f93bb427f2e31418a17ffa8b7c
stamovm <m.stamov@yahoo.com>
2021-12-06 21:52:31 -0800

    added ball movement animation

--- Ball.js
+++ Ball.js
@@ -1,9 +1,62 @@
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.000005
+
 export default class Ball {
   constructor(ballElement) {
     this.ballElement = ballElement
+    this.reset()
+  }
+
+  get x() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue('--x')
+    )
+  }
+  set x(val) {
+    this.ballElement.style.setProperty('--x', val)
+  }
+
+  get y() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue('--y')
+    )
+  }
+  set y(val) {
+    this.ballElement.style.setProperty('--y', val)
+  }
+
+  rect() {
+    return this.ballElement.getBoundingClientRect()
+  }
+
+  reset() {
+    this.x = 50
+    this.y = 50
+    this.direction = { x: 0 }
+    while (
+      Math.abs(this.direction.x) <= 0.2 ||
+      Math.abs(this.direction.y) >= 0.9
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI)
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+    }
+    this.velocity = INITIAL_VELOCITY
   }
 
   update(delta) {
-    //todo 15:30
+    this.x += this.direction.x * this.velocity * delta
+    this.y += this.direction.y * this.velocity * delta
+    this.velocity += VELOCITY_INCREASE * delta
+    const rect = this.rect()
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1
+    }
+    if (rect.left <= 0 || rect.right >= window.innerWidth) {
+      this.direction.x *= -1
+    }
   }
+}
+
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min
 }
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 import Ball from './Ball.js'
 
-const ball = new Ball(document.getElementById(Ball))
+const ball = new Ball(document.getElementById('ball'))
+//todo 27:15
 
 let lastTime
-
 function update(time) {
   if (lastTime != null) {
     const delta = time - lastTime
-    ball.update(delta)
+    // ball.update(delta)
   }
   lastTime = time
   window.requestAnimationFrame(update)
 }
-// window.requestAnimationFrame(update)
+window.requestAnimationFrame(update)
+console.log(ball)
